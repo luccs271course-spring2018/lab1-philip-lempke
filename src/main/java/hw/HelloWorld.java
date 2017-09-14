f6ce0f9b85ef61ee816b909ffdf582ae54f66c1a
Philip Lempke <plempke@luc.edu>
2017-09-14 00:27:08 +0000

    lab1-part4

--- src/main/java/hw/HelloWorld.java
+++ src/main/java/hw/HelloWorld.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class HelloWorld {
   
   
-  public static Object[] getFizzBuzzArray(int n){
+  public static List<String> getFizzBuzzList(int n){
     
      // int n = 17;
         
@@ -22,24 +22,63 @@ public class HelloWorld {
                // System.out.println(String.valueOf(int(i));
            // }
        
-    Object[] array;
-    array = new Object[n];
+   //Object[] array;
+    //array = new Object[n];
     
-    for(int i = 1; i <= array.length; i++){
+    //for(int i = 1; i <= array.length; i++){
+      //if (i%15 == 0){
+        //array[i-1] = "FizzBuzz";
+      //}
+      //else if(i%5 == 0){
+        //array[i-1] = "Buzz";
+      //}
+      //else if(i%3 == 0){
+        //array[i-1] = "Fizz";
+      //}
+      //else{
+        //array[i-1] = i;
+      //}
+    //}
+    //return array;
+    
+    //Object[] array;
+    //array = new Object[n];
+    
+    //for(int i = 1; i <= array.length; i++){
+      //if (i%15 == 0){
+        //array[i-1] = "FizzBuzz";
+      //}
+      //else if(i%5 == 0){
+        //array[i-1] = "Buzz";
+      //}
+      //else if(i%3 == 0){
+        //array[i-1] = "Fizz";
+      //}
+      //else{
+        //array[i-1] = i;
+      //}
+    //}
+    //return array;
+  
+    List<String> list = new arrayList<String>(n);
+    
+    for(int i = 1; i <= list.length; i++){
       if (i%15 == 0){
-        array[i-1] = "FizzBuzz";
+        list[i-1] = "FizzBuzz";
       }
       else if(i%5 == 0){
-        array[i-1] = "Buzz";
+        list[i-1] = "Buzz";
       }
       else if(i%3 == 0){
-        array[i-1] = "Fizz";
+        list[i-1] = "Fizz";
       }
       else{
-        array[i-1] = i;
+        list[i-1] = i;
       }
     }
-    return array;
+    return list;
+    
+    
   }
 }
 
